a91d52708250b53e6de8970600f669cd580ca956
Venilson Neves <103954028+venilson0708@users.noreply.github.com>
2024-12-27 10:54:18 -0300

    Cria a entidade Inquilino e seus atributos

--- src/main/java/com/venilson/kitnet/entidades/Inquilino.java
+++ src/main/java/com/venilson/kitnet/entidades/Inquilino.java
@@ -1,4 +1,23 @@
 package com.venilson.kitnet.entidades;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Inquilino {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private LocalDateTime dataDeNascimento;
+    private String contato;
+    private String email;
 }
